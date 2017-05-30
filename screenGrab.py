5b37cb1538df91b20cd083290c64ff4f04df53c7
Ka Tai Ho <kataihoo@gmail.com>
2017-05-29 18:00:31 -0700

    added new screen

--- screenGrab.py
+++ screenGrab.py
@@ -3,15 +3,20 @@ from PIL import ImageGrab
 import cv2
 import time
 
+def process_img(original_image):
+	processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+	processed_img = cv2.Canny(processed_img, threshold1 = 200, threshold2 = 300)
+	return processed_img
 
 last_time = time.time()
 while(True):
 	screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-	# printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8')\
-	# .reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
+	new_screen = processed_img(screen)
+
 	print('Loop took ' + str(time.time() - last_time))
 	last_time = time.time()
-	cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+	cv2.imshow('window2', cv2.cvtColor(screen, new_screen))
+	# cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
 	if cv2.waitKey(25) & 0xFF == ord('q'):
 		cv2.destroyAllWindows()
 		break
