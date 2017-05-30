8092a6816037e10fee990bb5afcf5bc6eef2d85c
Ka Tai Ho <kataihoo@gmail.com>
2017-05-29 20:24:21 -0700

    added new screen

--- screenGrab.py
+++ screenGrab.py
@@ -17,21 +17,21 @@ def process_img(original_image):
 	processed_img = cv2.Canny(processed_img, threshold1 = 200, threshold2 = 300)
 	return processed_img
 
-def run():
-	time.sleep(4)
 
+time.sleep(4)
+
+last_time = time.time()
+print (type(movement_dict['W']))
+
+while(True):
+	#PressKey(movement_dict['W'])
+	screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
+	new_screen = process_img(screen)
+
+	print('Loop took ' + str(time.time() - last_time))
 	last_time = time.time()
-	print (type(movement_dict['W']))
-
-	while(True):
-		PressKey(movement_dict['W'])
-		screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-		new_screen = process_img(screen)
-
-		print('Loop took ' + str(time.time() - last_time))
-		last_time = time.time()
-		cv2.imshow('window', new_screen)
-		# cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-		if cv2.waitKey(25) & 0xFF == ord('q'):
-			cv2.destroyAllWindows()
-			break
+	cv2.imshow('window', new_screen)
+	# cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+	if cv2.waitKey(25) & 0xFF == ord('q'):
+		cv2.destroyAllWindows()
+		break
