013002ff413abc47766faae237884279cdcc195b
Ka Tai Ho <kataihoo@gmail.com>
2017-05-30 22:21:59 -0700

    filtering data

--- screenGrab.py
+++ screenGrab.py
@@ -7,6 +7,14 @@ import pyautogui
 
 movement_dict = {'W' : 0x11, 'A' : 0x1E, 'S' : 0x1f, 'D' : 0x20}
 
+def draw_lines(img, lines):
+	try:
+		for line in lines:
+			coords = line[0]
+			cv2.line(img, (coords[0], (coords[1])), (coords[2], coords[3]), [255, 255, 255], 3)
+	except:
+		pass
+
 def region_of_interest(img, vertices):
 	mask = np.zeros_like(img)
 	cv2.fillPoly(mask, vertices, 255)
@@ -17,8 +25,13 @@ def process_img(original_image):
 	# processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
 	processed_img = original_image
 	processed_img = cv2.Canny(processed_img, threshold1 = 200, threshold2 = 300)
+	processed_img = cv2.GaussianBlur(processed_img, (5,5), 0)
 	vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]], np.int32)
 	processed_img = region_of_interest(processed_img, [vertices])
+
+	#edges
+	lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, 20, 15)
+	draw_lines(processed_img, lines)
 	return processed_img
 
 time.sleep(4)
